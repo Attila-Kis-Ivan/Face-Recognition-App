28100670c25f7fa172f62899d2fa32897d4a5a8d
TDB <attila.kisivan@gmail.com>
2023-01-07 15:47:28 +0000

    created app front end

--- src/App.js
+++ src/App.js
@@ -1,41 +1,97 @@
 import React, {Component} from 'react';
 import Navigation from './components/Navigation/Navigation';
-import ParticlesBg from 'particles-bg'
-import Logo from './components/Logo/Logo'
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import Signin from './components/Signin/Signin';
+import Register from './components/Register/Register';
+import Clarifai from 'clarifai';
+import ParticlesBg from 'particles-bg';
+import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 
 import './App.css';
 
+const app = new Clarifai.App({
+  apiKey: '629ce7fcabe9460fb103644a5591ff37'
+ });
+
 class App extends Component {
   constructor(){
     super();
     this.state = {
-      input: ''
+      input: '',
+      imageUrl: '',
+      box: {},
+      route: 'signin',
+      isSignedIn: false
     }
   }
 
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.region[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    this.setState({box: box});
+  }
+
   onInputchange= (event) => {
-    console.log(event.target.value);
+    this.setState({input: event.target.value});
   } 
 
   onButtonSubmit = () => {
-    console.log('click')
+   this.setState({imageUrl: this.state.input})
+    app.models
+      .predict(
+        Clarifai.FACE_DETECT_MODEL,
+        this.state.input)
+      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .catch (err => console.log(err));
+  }
+
+  onRouteChange = (route) => {
+    if (route === 'signout') {
+      this.setState({isSignedIn: false})
+    } else if(route === 'home') {
+      this.setState({isSignedIn: true})
+    }
+    this.setState({route: route});
   }
 
   render() {
+    const {isSignedIn, imageUrl, route, box} = this.state;
   return (
     <div className="App">
-    <ParticlesBg color="#ffffff" type="cobweb" num={200} bg={true} />
-      <Navigation />
-      <Logo/>
-      <Rank/>
-     <ImageLinkForm 
-       onInputchange={this.onInputchange} 
-       onButtonSubmit={this.onButtonSubmit}
-      />
+    <ParticlesBg color="#ffffff" type="cobweb" num={200} bg={true} 
+    />
+      <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
+      {route === 'home' 
+        ? <div>
+            <Logo/>
+            <Rank/>
+            <ImageLinkForm 
+            onInputchange={this.onInputchange} 
+            onButtonSubmit={this.onButtonSubmit}
+            />
+            <FaceRecognition box={box} imageUrl={imageUrl}/> 
+            </div>
+            : (
+              route === 'signin'  
+              ? <Signin onRouteChange={this.onRouteChange}/>
+              : <Register onRouteChange={this.onRouteChange}/>
+             
+            )
 
-       {/* <FaceRrcognition/>*/}
+      }
     </div>
    );
  }
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,12 +1,24 @@
 import React from "react";
 import './Navigation.css';
 
-const Navigation = () => {
-    return(
+const Navigation = ({onRouteChange, isSignedIn}) => {
+  
+      if(isSignedIn) {
+        return(
         <nav>
-          <p id="sign-out">Sign Out</p>
+          <p onClick={() => onRouteChange('signout')} id="sign-out">Sign Out</p>
         </nav>
-    )
+        );
+      } else {
+      return (
+       <nav>
+          <p onClick={() => onRouteChange('signin')} id="sign-in">Sign In</p>
+        
+          <p onClick={() => onRouteChange('register')} id="register">Register</p>
+       </nav>
+
+      );
+    }
 }
 
 export default Navigation;
