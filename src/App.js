3557c09ffb19e5ff04391d1be1f1902ebaca2728
TDB <attila.kisivan@gmail.com>
2023-01-15 17:15:17 +0000

    fixed name issue and entries

--- src/App.js
+++ src/App.js
@@ -76,7 +76,22 @@ class App extends Component {
       version: "34ce21a40cc24b6b96ffee54aabff139"
     },
       this.state.input)
-      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .then(response => {
+        if (response) {
+          fetch('http://localhost:3000/image', {
+            method: 'put',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+            id: this.state.user.id,
+      })
+          })
+          .then(response => response.json())
+          .then(count => {
+            this.setState(Object.assign(this.state.user, {entries: count}))
+          })
+        }
+        this.displayFaceBox(this.calculateFaceLocation(response))
+      })
       .catch (err => console.log(err));
   }
 
